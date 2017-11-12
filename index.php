06a337e47587fcba819a28c1a99e09f625e81a31
dernawy <akram.latrash@gmail.com>
2017-11-12 19:59:34 +0200

    Add files via upload
    upload index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LIBCASTING
+ * Date: 11/12/2017
+ * Time: 1:08 PM
+ */
+?>
+
+<html>
+<header>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.7.1.min.js"></script>
+
+</header>
+<body>
+<div id="results"></div>
+</body>
+<script src="JS/print_json.js"></script>
+</html>
